7880955fdd2e26ecbcb1e51dca29f3c2b3022c38
ezTriper <wanter438@gmail.com>
2024-12-18 03:56:12 +0300

    Update project files

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,12 +2,19 @@ import React from "react";
 
 export function AboutPage() {
 	return (
-		<div className="w-full h-full p-10 ">
-			<div className="flex flex-col bg-black bg-opacity-20 text-white shadow-lg rounded-lg p-6 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl border border-blue-500">
+		<div className="w-full h-full p-10 backdrop-blur-3xl bg-blue-100 bg-opacity-20 ">
+			<div className="flex flex-col bg-black bg-opacity-20 text-gray-200 drop-shadow-md rounded-lg p-6 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl border border-blue-600  hover:bg-opacity-50 hover:border-blue-500 hover:text-white hover:drop-shadow-lg transition ease-in-out duration-500 cursor-pointer transition-colors wrap-card relative">
+				<span className="absolute flex h-3 w-3 -top-1 -right-1">
+					<span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+					<span className="relative inline-flex rounded-full h-3 w-8 bg-green-500"></span>
+				</span>
 				<h2 className="text-2xl sm:text-3xl font-bold  mb-4">
 					Заголовок второй странички
 				</h2>
-				<p className="mb-4">Сосал?</p>
+				<p className="mb-4">
+					Это пример текстового содержимого, оформленного с помощью Tailwind
+					CSS.
+				</p>
 			</div>
 		</div>
 	);
--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -2,17 +2,26 @@ import React from "react";
 
 export function ProductsPage() {
 	return (
-		<div className="w-full h-full p-10 backdrop-blur-3xl bg-blue-100 bg-opacity-20 ">
-			<div className="flex flex-col bg-black bg-opacity-20 text-gray-200 drop-shadow-md rounded-lg p-6 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl border border-blue-600  hover:bg-opacity-50 hover:border-blue-500 hover:text-white hover:drop-shadow-lg transition ease-in-out duration-500 cursor-pointer transition-colors wrap-card relative">
-				<span className="absolute flex h-3 w-3 -top-1 -right-1">
-					<span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-					<span className="relative inline-flex rounded-full h-3 w-8 bg-green-500"></span>
-				</span>
-				<h2 className="text-2xl sm:text-3xl font-bold  mb-4">Заголовок</h2>
-				<p className="mb-4">
-					Это пример текстового содержимого, оформленного с помощью Tailwind
-					CSS.
-				</p>
+		<div className="w-full h-full p-10 backdrop-blur-3xl bg-blue-100 bg-opacity-20">
+			<div className="card flex flex-col bg-black bg-opacity-20 text-gray-200 drop-shadow-md rounded-lg p-6 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl border border-blue-600 relative overflow-hidden">
+				{/* Основной контент */}
+				<div className="card-content1 absolute top-0 left-0 w-full h-full flex flex-col justify-center transition-transform duration-500 ease-in-out">
+					<h2 className="text-2xl sm:text-3xl font-bold mb-4">Заголовок</h2>
+					<p className="mb-4">
+						Это пример текстового содержимого, оформленного с помощью Tailwind
+						CSS.
+					</p>
+				</div>
+				{/* Всплывающий контент */}
+				<div className="card-content absolute top-full left-0 w-full h-full flex flex-col justify-center bg-opacity-40 bg-gray-800 text-white transition-transform duration-500 ease-in-out">
+					<h2 className="text-2xl sm:text-3xl font-bold mb-4">
+						Заголовок всплывашки
+					</h2>
+					<p className="mb-4">
+						Это пример текстового содержимого, оформленного с помощью Tailwind
+						CSS.
+					</p>
+				</div>
 			</div>
 		</div>
 	);
