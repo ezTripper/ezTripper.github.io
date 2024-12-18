75204f7391934c69718ff1a84c3aaab516ab98bb
ezTriper <wanter438@gmail.com>
2024-12-18 04:20:49 +0300

    Update project files

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,14 +4,14 @@ import {Link} from "react-router-dom";
 export function Navigation() {
 	return (
 		<nav className="h-[50px] flex justify-between px-5 bacdrop-filter backdrop-brightness-15  backdrop-blur-3xl items-center text-white bg-gray-800 bg-opacity-30 ">
-			<span className="font-bold">
+			<span className="font-bold text-2xl">
 				<Link
 					to="/"
 					className="mr-2">
 					ezTripper
 				</Link>
 			</span>
-			<span>
+			<span className="text-2xl">
 				<Link to="/about">Обо мне</Link>
 			</span>
 		</nav>
